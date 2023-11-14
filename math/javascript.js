59e2cb785c3b4219b6730783c74095c49eeccbfb
Ludi <128106403+LudimilaTSilva@users.noreply.github.com>
2023-11-14 08:48:44 -0300

    Add files via upload

--- math/javascript.js
+++ math/javascript.js
@@ -1,9 +1,22 @@
-function fatorial_user() {
+function conta() {
+    let campo = n;
+
     let n = parseInt(campo.value);
     let fat = n;
+    let pot = 0;
+    let soma = 0;
+    let i = n;
 
-    while (n>1) {
-        fat = fat*(n-1);
+    while (n>=1) {
+        pote = Math.pow(2,n);
+        i = n;
+        while (i>1) {
+            fat = fat*(i-1);
+            i = i-1;
+        }
+        soma = soma+(pote/fat);
         n = n-1;
     }
+    soma = soma*(1/3);
+    alert(soma);
 }
